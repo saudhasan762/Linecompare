36087c1cc4366c39111ed754d2ecbdaf43526b2a
saudhasan762 <azanali152@gmail.com>
2021-02-14 23:26:56 +0530

    Added method for Calculating length

--- LineComparision.java
+++ LineComparision.java
@@ -2,30 +2,33 @@ import java.util.*;
 public class LineComparision {
 
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter values of Line1 x1,y1 x2,y2");
-		int x1 = scan.nextInt();
-		int y1 = scan.nextInt();
-		int x2 = scan.nextInt();
-		int y2 = scan.nextInt();
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter coordinates of Line1");
+		double a = scan.nextDouble();
+		double b = scan.nextDouble();
+		double c = scan.nextDouble();
+		double d = scan.nextDouble();
+		double lengthOfLine1 = calculate(a,b,c,d);
 		
-		double length_line1=Math.sqrt((x2 - x1)^2 + (y2-y1)^2);
-		System.out.println("lenght of line1 " +length_line1);
+		System.out.println("Enter coordinates of line2");
+		double e = scan.nextDouble();
+		double f = scan.nextDouble();
+		double g = scan.nextDouble();
+		double h = scan.nextDouble();
+		double lengthOfLine2 = calculate(e,f,g,h);
 		
-		System.out.println("Enter values of Line2 u1,v1 u2,v2");
-		int u1 = scan.nextInt();
-		int v1 = scan.nextInt();
-		int u2 = scan.nextInt();
-		int v2 = scan.nextInt();
-		double length_line2=Math.sqrt((u2 - u1)^2 + (v2 - v1)^2);
-		System.out.println("Lenght of line2 "+length_line2);
-
+		System.out.println("length of line1 " +lengthOfLine1);
+		System.out.println("length of line2 " +lengthOfLine2);
 		
-		if ( length_line1 == length_line2 ) {
-			System.out.println("Both lines are equal");	
-		}
+		if ( lengthOfLine1 == lengthOfLine2 )
+			System.out.println("length of both lines are equal");	
 		else
-			System.out.println("lines are not equal");
-
+			System.out.println("length of lines are not equal");
+	}
+	public static double calculate(double x1,double y1, double x2, double y2) {
+		double length = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+		return length;
 	}
 }
+
+
